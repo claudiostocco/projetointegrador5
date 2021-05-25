79ab60e6b08a33ce5c86689eff33bfd5a408cdf9
developer 203 <claudiostocco@gmail.com>
2021-05-25 17:58:36 -0300

    Cadastro de questões

--- pages/newQuestion/index.js
+++ pages/newQuestion/index.js
@@ -1,12 +1,10 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import styled from 'styled-components'
 import db from '../../db.json';
 import Widget from '../../src/components/Widget';
 import QuizLogo from '../../src/components/QuizLogo';
 import QuizBackground from '../../src/components/QuizBackground';
 import QuizContainer from '../../src/components/QuizContainer';
-import Button from '../../src/components/Button';
 import BackLinkArrow from '../../src/components/BackLinkArrow';
 import CadastroQuestao from '../../src/components/CadastroQuestao';
 
@@ -21,20 +19,20 @@ export default function NewQuestion() {
                   <BackLinkArrow href='/' />
                   Incluir nova questão
               </Widget.Header>
-              <CadastroQuestao>
-                <Widget.Content>
+              <CadastroQuestao/>
+                {/* <Widget.Content>
                     <CadastroQuestao.Questao/>
                 </Widget.Content>  
                 <Widget.Content>
                     <CadastroQuestao.Opcoes/>
-                </Widget.Content>  
-              </CadastroQuestao>
+                </Widget.Content>   */}
+              {/* </CadastroQuestao> */}
             </Widget>
-            <Widget>
+            {/* <Widget>
               <Widget.Content>
                 <CadastroQuestao.Botao/>
               </Widget.Content>  
-            </Widget>
+            </Widget> */}
           </QuizContainer>
         </QuizBackground>
         )
--- src/components/CadastroQuestao/index.js
+++ src/components/CadastroQuestao/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import Button from '../Button';
+import Button from '../Button'
 import Input from '../Input'
+import Widget from '../Widget'
+import dbQ from '../../../dbQuestions.json'
 
 const CaixaFlex = styled.div`
                     display: flex;
@@ -12,70 +14,65 @@ const CaixaFlex = styled.div`
 
 
 const CadastroQuestao = ({ children, ...props }) => {
-    return (
-        <div>
-            {children}
-        </div>
-    )
-}
 
-CadastroQuestao.Questao = () => {
+    const Botao = ({handleIncluir}) => {
+        return (
+            <CaixaFlex>
+                <Button type='submit' onClick={handleIncluir}>Incluir</Button>
+            </CaixaFlex>
+        )
+    }
+
     const [titulo, setTitulo] = React.useState('')
     const [descricao, setDescricao] = React.useState('')
     const [urlImg, setUrlImg] = React.useState('')
-    const [resposta, setResposta] = React.useState()
+    const [resposta, setResposta] = React.useState('')
     const handleSetResposta = event => {
         const resp = Number.parseInt(event.target.value)
         Number.isInteger(resp) ?
-            ((resp > 0 && resp < 5) ? setResposta(resp) : alert('Valor deve ser entre 1 e 4!'))
+            ((resp > 0 && resp < 5) ? setResposta(event.target.value) : alert('Valor deve ser entre 1 e 4!'))
             :
             alert('Valor inválido!')
     }
-    return (
-        <div>
-            <Input name="Titulo" onChange={event => setTitulo(event.target.value)} placeholder="Titulo para a questão" value={titulo} />
-            <Input name="Descricao" onChange={event => setDescricao(event.target.value)} placeholder="Descrição para a questão" value={descricao} />
-            <Input name="UrlImg" onChange={event => setUrlImg(event.target.value)} placeholder="URL da imagem para a questão" value={urlImg} />
-            <Input name="Resposta" onChange={handleSetResposta} placeholder="Indice para a resposta da questão" value={resposta} />
-            <hr />
-        </div>
-    )
-}
-
-CadastroQuestao.Opcoes = () => {
-    const [opcoes, setOpcoes] = React.useState(["", "", "", "", ""])
+    const [opcoes, setOpcoes] = React.useState(["", "", "", ""])
     const handleNovaOp = (e, i) => {
-        console.log('antes', opcoes)
         const opcoesNova = [...opcoes]
         opcoesNova[i] = e.target.value
         setOpcoes(opcoesNova)
     }
+
+    const handleIncluir = () => {
+        console.log('Titulo: ',titulo,'\nResposta: ',resposta,'\nOpções: ',opcoes)
+        // const newQ = {
+        //     image: urlImg,
+        //     title: titulo,
+        //     description: descricao,
+        //     answer: resposta,
+        //     alternatives: [
+            
+        //     ]
+        // }
+    }
+
     return (
         <div>
-            <Input name={`Op0`} onChange={event => handleNovaOp(event, 0)} placeholder={`Opção 1`} value={opcoes[0]} />
-            <Input name={`Op1`} onChange={event => handleNovaOp(event, 1)} placeholder={`Opção 2`} value={opcoes[1]} />
-            <Input name={`Op2`} onChange={event => handleNovaOp(event, 2)} placeholder={`Opção 3`} value={opcoes[2]} />
-            <Input name={`Op3`} onChange={event => handleNovaOp(event, 3)} placeholder={`Opção 4`} value={opcoes[3]} />
-            <Input name={`Op4`} onChange={event => handleNovaOp(event, 4)} placeholder={`Opção 5`} value={opcoes[4]} />
-
-            {/* {console.log('div ->',opcoes)}
-                {opcoes.forEach((e,i) => {
-                    {console.log('Input ->',e)}
-                    <Input name={`Op${i}`} onChange={event => handleNovaOp(event,i)} placeholder={`Opção ${i+1}`} value={opcoes[i]}/>
-                })} */}
+            <Widget.Content>
+                <Input name="Titulo" onChange={event => setTitulo(event.target.value)} placeholder="Titulo para a questão" value={titulo} />
+                <Input name="Descricao" onChange={event => setDescricao(event.target.value)} placeholder="Descrição para a questão" value={descricao} />
+                <Input name="UrlImg" onChange={event => setUrlImg(event.target.value)} placeholder="URL da imagem para a questão" value={urlImg} />
+                <Input name="Resposta" onChange={handleSetResposta} placeholder="Indice para a resposta da questão" value={resposta} />
+            </Widget.Content>
+            <Widget.Content>
+                <Input name={`Op0`} onChange={event => handleNovaOp(event, 0)} placeholder={`Opção 1`} value={opcoes[0]} />
+                <Input name={`Op1`} onChange={event => handleNovaOp(event, 1)} placeholder={`Opção 2`} value={opcoes[1]} />
+                <Input name={`Op2`} onChange={event => handleNovaOp(event, 2)} placeholder={`Opção 3`} value={opcoes[2]} />
+                <Input name={`Op3`} onChange={event => handleNovaOp(event, 3)} placeholder={`Opção 4`} value={opcoes[3]} />
+            </Widget.Content>
+            <Widget.Content>
+                <Botao handleIncluir={handleIncluir}/>
+            </Widget.Content>
         </div>
     )
 }
-
-CadastroQuestao.Botao = () => {
-    const handleIncluir = () => {
-        console.log('')
-    }
-    return (
-        <CaixaFlex>
-            <Button type='submit'>Incluir</Button>
-        </CaixaFlex>
-    )
-}
-
+// https://imasters.com.br/front-end/simplificando-componentes-com-react-hooks
 export default CadastroQuestao;
